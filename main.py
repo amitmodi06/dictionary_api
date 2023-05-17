d941c71e781181b789a196c206d3cc6588a1d748
amitmodi06 <amitmodi06@gmail.com>
2023-05-17 23:37:21 +0100

    Corrected port and cleaned the code

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 
-
 my_app = Flask(__name__)
+
 
 @my_app.route("/")
 def home_page():
@@ -10,8 +10,10 @@ def home_page():
 
 @my_app.route("/app/v1/<word>")
 def translator_page(word):
-    return {"definition": word.upper(),
-            "word": word}
+    definition = word.upper()
+    result_dictionary = {"word": word, "definition": definition}
+    return result_dictionary
+
 
 if __name__ == "__main__":
     my_app.run(debug=True, port=5001)
